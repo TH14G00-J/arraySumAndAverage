66a145f4bef635e3c6fa0f18d53a74604114031b
Thiago <tjose.corp@gmail.com>
2025-08-12 19:04:59 -0300

    Implementa programa em Java que lê N números reais, armazena em vetor, exibe os valores digitados, calcula a soma e a média, utilizando Scanner, for e for-each.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Quantos números você vai digitar? ");
+        int n = sc.nextInt();
+
+        double[] vector = new double[n];
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print("Digite um número: ");
+            vector[i] = sc.nextDouble();
         }
+
+        double sum = 0.0;
+        System.out.print("VALORES =");
+        for (double j : vector) {
+            System.out.printf(" %.1f", j);
+            sum += j;
+        }
+
+        double avg = sum / vector.length;
+        System.out.printf("%nSOMA: %.1f%n", sum);
+        System.out.printf("MEDIA: %.1f", avg);
     }
 }
